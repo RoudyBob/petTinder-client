59d8b837b706c2e71448a0e96b6fd8dfc2e39920
Bob Roudebush <bob@roudybob.net>
2021-02-21 13:40:42 -0500

    added search capabilities

--- src/components/Homepage/PetSwipe.js
+++ src/components/Homepage/PetSwipe.js
@@ -1,77 +1,129 @@
 import React, { useState, useEffect } from 'react';
-import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption, Form, Button} from 'reactstrap';
-
+import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption, Input, Form, Button} from 'reactstrap';
 
 const PetSwipe = (props) => {
     const [allPets, setAllPets] = useState([]);
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
+    const [searchGender, setSearchGender] = useState('Both');
+    const [searchCity, setSearchCity] = useState('');
 
     useEffect(() => {
-        fetchPets();
-    }, []);
+        fetchPets(searchGender,searchCity,'');
+    },[]);
 
   const next = () => {
-    if (animating) return;
     const nextIndex = activeIndex === allPets.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
   const previous = () => {
-    if (animating) return;
     const nextIndex = activeIndex === 0 ? allPets.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   }
 
   const goToIndex = (newIndex) => {
-    if (animating) return;
     setActiveIndex(newIndex);
   } 
 
-    const fetchPets = () => {
-        console.log('hi there');
-        fetch('http://localhost:3000/pet', {
+    const fetchPets = (gender, city, state) => {
+        let url = '';
+
+        if (gender === 'Male') {
+            (city) ? url = `http://localhost:3000/pet/city/${city}/${gender}` : url = `http://localhost:3000/pet/gender/${gender}`
+        } else if (gender === 'Female') {
+            (city) ? url = `http://localhost:3000/pet/city/${city}/${gender}` : url = `http://localhost:3000/pet/gender/${gender}`
+        } else {
+            (city) ? url = `http://localhost:3000/pet/city/${city}/` : url = `http://localhost:3000/pet/`
+        } 
+
+        console.log(`fetch url: ${url}`);
+        fetch(url, {
             method: 'GET',
             headers: new Headers ({
                 'Content-Type': 'application/json',
                 'Authorization': props.token
+            })
         })
-    }).then((res) => res.json())
-    .then((petAll) => {
-        setAllPets(petAll)
-    })
-}
+        .then((res) => res.json())
+        .then((petAll) => { setAllPets(petAll) })
+    }
 
-
-
-const slides = () => {
-    return allPets.map((pet) => {
-    return (
-        <CarouselItem key={pet.id}>
+    const slides = () => {
+        return allPets.map((pet) => {
+        return (
+            <CarouselItem key={pet.id}>
+                
+                <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" style={{height: 400 + 'px', width: 'auto'}}/>
+                
+                <CarouselCaption captionText={pet.dogname} captionHeader={pet.breed} />  
+                <div className="pet-carousel">
+                    {pet.gender}<br></br>
+                    {pet.citylocation}<br></br>
+                    {pet.statelocation}<br></br>
+                    {pet.description}
+                </div>   
+            </CarouselItem>
             
-            <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" style={{height: 400 + 'px', width: 'auto'}}/>
-            
-            <CarouselCaption captionText={pet.dogname} captionHeader={pet.breed} />  
-            <div className="pet-carousel">
-                {pet.gender}<br></br>
-                {pet.citylocation}<br></br>
-                {pet.statelocation}<br></br>
-                {pet.description}
-            </div>   
-        </CarouselItem>
-        
-    );
-})};
+        );
+    })};
 
+    const genderSelection = (e) => {
+        let gender = e.target.value;
+        console.log(`Looking for all ${gender} dogs.`);
+        setSearchGender(gender);
+        fetchPets(gender, searchCity,'');
+    }
+
+    const citySelection = (e) => {
+        e.preventDefault();
+        console.log(`Looking for ${searchGender} dogs in ${searchCity}.`);
+        setSearchCity(searchCity);
+        fetchPets(searchGender, searchCity,'');
+    }
+
+    const clearFilters = (e) => {
+        let gender = 'Both';
+        let city = null;
+        setSearchGender('Both');
+        setSearchCity('');
+        fetchPets(gender, '','');
+
+    }
 
     return ( 
         
-        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-            <CarouselIndicators items={allPets} activeIndex={activeIndex} onClickHandler={goToIndex}/>
-            {slides()}
-            <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-            <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-        </Carousel>
+        <div>
+            <Carousel interval={false} activeIndex={activeIndex} next={next} previous={previous}>
+                <CarouselIndicators items={allPets} activeIndex={activeIndex} onClickHandler={goToIndex}/>
+                {slides()}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+            </Carousel>
+
+            <div className="searchContainer">
+                <div className="genderFilter">
+                    <h5>Filter by Gender</h5>
+                    <input type="radio" name="gender" value="Male" checked={searchGender === "Male"} onChange = {genderSelection}></input>
+                    <label htmlFor="male">Male</label><br />
+                    <input type="radio" name="gender" value="Female" checked={searchGender === "Female"} onChange = {genderSelection}></input>
+                    <label htmlFor="female">Female</label><br />
+                    <input type="radio" name="gender" value="Both" checked={searchGender === "Both"} onChange = {genderSelection}></input>
+                    <label htmlFor="both">Both</label>
+                </div>
+
+                <div className="cityFilter">
+                    <h5>Filter by City</h5>
+                    <form onSubmit={citySelection}>
+                        <input type="text" name="gender" autocomplete="off" value={searchCity} placeholder="City" onChange={(e) => setSearchCity(e.target.value)}></input>
+                        <button type="submit" value="Submit">submit</button>
+                    </form>
+                </div>
+            </div>
+            <div className="clearFilter">
+                <button type="button" onClick={clearFilters}>Clear Search Filters</button>
+            </div>
+        </div>
         
      ); 
 }
--- src/components/Pets/PetEdit.js
+++ src/components/Pets/PetEdit.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import axios from 'axios';
 
 const PetEdit = (props) => {
     const [editName, setEditName] = useState(props.petToUpdate.dogname);
@@ -8,9 +9,13 @@ const PetEdit = (props) => {
     const [editDescription, setEditDescription] = useState(props.petToUpdate.description);
     const [editPhotoUrl, setEditPhotoUrl] = useState(props.petToUpdate.photourl);
     
+    const [file, setFile] = useState(''); // storing the uploaded file  
+    const [data, getFile] = useState({ name: "", path: "" }); // storing the recived file from backend
+    const [fileInputKey, setFileInputKey] = useState('');
 
     const petUpdate = (event, pet) => {
         event.preventDefault();
+        uploadFile();
         fetch(`http://localhost:3000/pet/${props.petToUpdate.id}`, {
             method: 'PUT',
             body: JSON.stringify({dogname: editName, citylocation: editCity, statelocation: editState, description: editDescription, photourl: editPhotoUrl}),
@@ -24,30 +29,71 @@ const PetEdit = (props) => {
         })
     }
 
+    const fileInputChange = (e) => {
+        const file = e.target.files[0]; // accessing file
+        // console.log("Old File");
+        // console.log(file);
+        
+        let regEx = /(?:\.([^.]+))?$/;
+        const {lastModified, lastModifiedDate, name, size, type, webkitRelativePath} = file;
+        let newFileName = Math.random().toString(36).substring(2) + "." + regEx.exec(name)[1];
+
+        // https://stackoverflow.com/questions/21720390/how-to-change-name-of-file-in-javascript-from-input-file
+        const newFile = new File ([file], newFileName, {
+            lastModified: lastModified,
+            lastModifiedDate: lastModifiedDate,
+            name: Math.random().toString(36).substring(2) + "." + regEx.exec(name)[1],
+            size: size,
+            type: type,
+            webkitRelativePath: webkitRelativePath
+        });
+
+        // console.log("New File");
+        // console.log(newFile);
+        setFile(newFile); // storing file
+        setEditPhotoUrl(`http://localhost:3000/${newFile.name}`);
+    }
+
+    const uploadFile = () => {
+        const formData = new FormData();   
+        // console.log(`photourl variable: ${photourl}`)
+        formData.append('file', file); // appending file
+        axios.post('http://localhost:3000/upload', formData)
+        .then(res => {
+            console.log(res);
+            getFile({ name: res.data.name, path: 'http://localhost:3000' + res.data.path })
+        })
+        .catch(err => console.log(err))}
+
     return(
         <Modal isOpen={true}>
             <ModalHeader>need to update some info?</ModalHeader>
             <ModalBody>
                 <Form onSubmit={petUpdate}>
                     <FormGroup>
-                        <Label htmlFor="dogname">Name:</Label>
+                        <Label htmlFor="dogname">Dog's Name</Label>
                         <Input name="name" value={editName} onChange={(e) => setEditName(e.target.value)}/>
                     </FormGroup>
                     <FormGroup>
-                        <Label htmlFor="citylocation">City:</Label>
+                        <Label htmlFor="citylocation">City</Label>
                         <Input name="city" value={editCity} onChange={(e) => setEditCity(e.target.value)}/>
                     </FormGroup>
                     <FormGroup>
-                        <Label htmlFor="statelocation">State:</Label>
-                        <Input name="state" value={editState} onChange={(e) => setEditState(e.target.value)} />
+                        <Label htmlFor="statelocation">State (2-letter abbr))</Label>
+                        <Input name="state" pattern="|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY|" value={editState} onChange={(e) => setEditState(e.target.value)} />
                     </FormGroup>
                     <FormGroup>
-                        <Label htmlFor="description">Description:</Label>
+                        <Label htmlFor="description">Description</Label>
                         <Input name="description" value={editDescription} onChange={(e) => setEditDescription(e.target.value)} />
                     </FormGroup>
                     <FormGroup>
-                        <Label htmlFor="photourl">Photo (URL):</Label>
-                        <Input name="photo" value={editPhotoUrl} onChange={(e) => setEditPhotoUrl(e.target.value)} />
+                        <img src={props.petToUpdate.photourl} width = "100px" />
+                        <br />
+                        <Label htmlFor="photourl">Current Photo</Label>
+
+                        {/* <Input name="photo" value={editPhotoUrl} onChange={(e) => setEditPhotoUrl(e.target.value)} /> */}
+
+                        <Input type="file" id="photopicker" accept="image/*" onChange={fileInputChange} key={fileInputKey} />
                     </FormGroup>
                     <Button type="submit">Submit</Button>
                 </Form>
